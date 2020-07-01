79dcb5044ca62ab6c078fb3548c7964141759263
Divyanshu Raj <divyanshuraj5050@gmail.com>
2020-07-01 12:22:34 +0530

    added firewall{conceptually saying}  to acess the endpoints

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import giveUser from '../../controllers/users/index.js'
 import authorizer from '../../helpers/authorizers/index.js'
+import {isAuth , isAdmin } from '../../helpers/authorizers/index.js'
 const router  = express.Router()
 
-router.get('/user/:id',authorizer(),function(req, res){
+router.get('/user/:id',authorizer(),isAuth,isAdmin,function(req, res){
     res.json(
         {
             user:req.user
